7ba02b1dc12b22e4082bf89c2e26009dcfc1cd6f
kodewala-academy <kodewala.academy@gmail.com>
2026-07-03 11:38:59 +0530

    jar and dependency creation

---
+++ 03-user-mgmt/src/com/amazon/user/UserType.java
@@ -0,0 +1,8 @@
+package com.amazon.user;
+
+public class UserType
+{
+	public static String RETAIL = "retail";
+	public static String RESELLER = "reseller";
+	public static String MARKETPLACE = "marketplace";
+}
---
+++ 04-account-mgmt/src/com/amazon/account/AccountMgmt.java
@@ -0,0 +1,17 @@
+package com.amazon.account;
+
+import com.amazon.user.UserMgmt;
+
+public class AccountMgmt
+{
+
+	public static void main(String[] args)
+	{
+		// i want to user mgmt module 
+		
+		UserMgmt um = new UserMgmt();
+		um.getUserInfo("marketplace");
+
+	}
+
+}
