1ab4f6cd8cb6471dc2f1e4d6ea7a29d5796884af
kodewala-academy <kodewala.academy@gmail.com>
2026-07-27 11:33:08 +0530

    bean scope and lifecycle

--- Spring-DI/src/main/java/com/amazon/di/setter/emp/Employee.java
+++ Spring-DI/src/main/java/com/amazon/di/setter/emp/Employee.java
@@ -2,6 +2,9 @@ package com.amazon.di.setter.emp;
 
 import com.amazon.di.setter.address.Address;
 
+import jakarta.annotation.PostConstruct;
+import jakarta.annotation.PreDestroy;
+
 public class Employee {
 
 	private int salary;
@@ -11,16 +14,29 @@ public class Employee {
 
 	public Employee(int salary, String name) {
 		super();
+		System.out.println("1. Employee.Employee() - constructor ");
 		this.salary = salary;
 		this.name = name;
 	}
 
 	public void setAddress(Address _address) {
+		System.out.println("2. Employee.Employee() - Dependecny Injection ");
 		this.address = _address;
 	}
 
 	public void printInfo() {
+		System.out.println("4. Employee.Employee() - Doing work ");
 		address.displayAddressInfo();
+	}
+
+	@PostConstruct
+	public void init() {
+		System.out.println("3. Employee.Employee() - init ");
+	}
+
+	@PreDestroy
+	public void cleanup() {
+		System.out.println("5. Employee.Employee() - doing clean up before destroy.");
 	}
 
 }
